1e975502897b30ba668fee02a9be65edbb88111c
xeeny <xeeny@wp.pl>
2020-11-14 00:19:47 +0100

    update dmenu/config.h

--- dmenu/config.h
+++ dmenu/config.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	               /* fg        bg       */
 	[SchemeNorm] = { "#FFFFFF", "#000000" },
-	[SchemeSel] =  { "#FFFFFF", "#FF0000" },
+	[SchemeSel] =  { "#FF0000", "#000000" },
 	[SchemeOut] =  { "#FFFFFF", "#FFFFFF" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
