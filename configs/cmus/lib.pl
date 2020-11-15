d7ae781bf8702deb97f9df98929d416d1e93d667
xeeny <xeeny@wp.pl>
2020-11-15 14:45:46 +0100

    add configs/cmus/

---
+++ configs/cmus/lib.pl
@@ -0,0 +1,7 @@
+/home/xeeny/storage/music/other/Eromanga-sensei Ending Full『TrySail - adrenaline!!!』.mp3
+/home/xeeny/storage/music/other/Gabriel DropOut Opening - Gabriel DropKick (Single).mp3
+/home/xeeny/storage/music/other/Happy Happy Friends.mp3
+/home/xeeny/storage/music/other/Happy Sugar Life OP Full【MAD Lyrics】.mp3
+/home/xeeny/storage/music/other/Jashin chan opening.mp3
+/home/xeeny/storage/music/other/KONOSUBA -God's blessing on this wonderful world! 2 - Opening _ Tomorrow.mp3
+/home/xeeny/storage/music/other/Konosuba! Season 2 - 'Red Battle' Megumin vs Yunyun (RAP BATTLE).mp3
