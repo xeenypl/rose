f3ef8d906c43534746124ea6d3462594734ae4d2
xeeny <xeeny@wp.pl>
2020-11-16 13:39:46 +0100

    update st/config.h

--- st/config.h
+++ st/config.h
@@ -204,8 +204,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD,              XK_Escape,      keyboard_select,{.i =  0} },
     
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = 1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = 1} },
+	{ TERMMOD,              XK_J,           kscrollup,      {.i = 1} },
+	{ TERMMOD,              XK_K,           kscrolldown,    {.i = 1} },
 };
 
 /*
